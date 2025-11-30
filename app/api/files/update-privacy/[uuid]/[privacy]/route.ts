c15e174f99a466365db17c5e2fe245c455abff39
elvett <egor.priymak.2005@gmail.com>
2025-11-30 16:01:28 +0200

    add update-privacy API endpoint

---
+++ app/api/files/update-privacy/[uuid]/[privacy]/route.ts
@@ -0,0 +1,66 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { getUserFromToken } from "@/lib/auth";
+
+interface FileShareParams {
+  params: {
+    uuid: string;
+    privacy: string;
+  };
+}
+
+interface FileShareSuccess {
+  message: string;
+}
+
+interface FileShareError {
+  error: string;
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: FileShareParams,
+): Promise<NextResponse<FileShareSuccess | FileShareError>> {
+  try {
+    const user = await getUserFromToken();
+    const userId = user?.userId;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const Params = await params;
+    const fileUuid = Params.uuid;
+    const letprivacy = Params.privacy === "true";
+
+    if (!fileUuid) {
+      return NextResponse.json({ error: "UUID is required" }, { status: 400 });
+    }
+
+    const file = await db.files.findUnique({
+      where: { uuid: fileUuid },
+      select: { ownerId: true },
+    });
+
+    if (!file) {
+      return NextResponse.json({ error: "File not found" }, { status: 404 });
+    }
+
+    if (file.ownerId !== userId) {
+      return NextResponse.json({ error: "Access denied" }, { status: 403 });
+    }
+
+    await db.files.update({
+      where: { uuid: fileUuid },
+      data: { private: letprivacy },
+    });
+
+
+   return NextResponse.json({ message: "File privacy update " + letprivacy });
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
